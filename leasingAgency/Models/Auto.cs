5f08ac12fb1ad2e03fcf0b22f6cf56c53eab0e3a
sergeyb348 <sergeyb348@gmail.com>
2021-04-13 21:57:53 +0300

    Основное окно

---
+++ leasingAgency/Models/Auto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace leasingAgency.Models
+{
+    class Auto : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string nameAuto
+        {
+            get { return nameAuto; }
+
+            set
+            {
+                nameAuto = value;
+                OnPropertyChanged("nameAuto");
+            }
+        }
+
+        public string priceAuto
+        {
+            get { return priceAuto; }
+
+            set
+            {
+                nameAuto = value;
+                OnPropertyChanged("priceAuto");
+            }
+        }
+
+        public string infoAuto
+        {
+            get { return infoAuto; }
+
+            set
+            {
+                infoAuto = value;
+                OnPropertyChanged("infoAuto");
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ leasingAgency/Views/LoginWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace leasingAgency.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MainWindowUser taskMainWindowUser = new MainWindowUser();
+            taskMainWindowUser.Show();
+            this.Close();
+        }
+    }
+}
